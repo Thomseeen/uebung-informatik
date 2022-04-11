984db9497c59e9e831c39087c6903f7e4a61b236
Thomas Wagner <thomas.wagner@pa-systems.de>
2022-04-11 08:48:41 +0200

    add example solution

---
+++ examples/examples-s22/lecture_examples.h
@@ -0,0 +1,7 @@
+#ifndef LECTURE_EXAMPLES_H
+#define LECTURE_EXAMPLES_H
+
+void lecture1_format_identifier();
+void lecture2_loops();
+
+#endif /* LECTURE_EXAMPLES_H */
---
+++ examples/examples-s22/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "lecture_examples.h"
+
+int main(int argc, char* argv[]) {
+	printf("Hello World!\n");
+
+	lecture1_format_identifier();
+	lecture2_loops();
+
+	printf("\n\nPress ENTER key to continue...\n");
+	char _ = getchar();
+
+	return 0;
+}
