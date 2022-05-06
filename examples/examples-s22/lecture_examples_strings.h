7b020d72b33fae93f5c90fc00ba5ebb14a4c8e2f
Thomas Wagner <thomas.wagner@pa-systems.de>
2022-05-06 15:41:15 +0200

    bump string examples

---
+++ examples/examples-s22/lecture_examples_strings.h
@@ -0,0 +1,12 @@
+#ifndef LECTURE_EXAMPLES_STRINGS_H
+#define LECTURE_EXAMPLES_STRINGS_H
+
+int is_alphanumeric(char cc);
+
+void examples_strings1();
+void examples_strings2();
+void examples_strings3();
+void examples_strings4();
+void examples_strings5();
+
+#endif /* LECTURE_EXAMPLES_STRINGS_H */
--- examples/examples-s22/main.c
+++ examples/examples-s22/main.c
@@ -2,6 +2,11 @@
 
 #include "lecture_examples.h"
 #include "lecture_examples_arrays.h"
+#include "lecture_examples_strings.h"
+
+void print_is_alphanumeric(char cc) {
+	printf("%c is alphanumeric? -> %s\n", cc, is_alphanumeric(cc) ? "yes" : "no");
+}
 
 int main(int argc, char* argv[]) {
 	printf("Hello World!\n");
@@ -10,7 +15,19 @@ int main(int argc, char* argv[]) {
 	//lecture2_loops();
 	//lecture3_strings();
 
-	examples_arrays5();
+	//examples_arrays5();
+
+	printf("C is alphanumeric? -> %d\n", is_alphanumeric('C'));
+	printf("q is alphanumeric? -> %d\n", is_alphanumeric('q'));
+	printf("3 is alphanumeric? -> %d\n", is_alphanumeric('3'));
+	printf("+ is alphanumeric? -> %d\n", is_alphanumeric('+'));
+
+	print_is_alphanumeric('C');
+	print_is_alphanumeric('q');
+	print_is_alphanumeric('3');
+	print_is_alphanumeric('+');
+
+	print_is_alphanumeric(48);
 
 	printf("\n\nPress ENTER key to continue...\n");
 	char _ = getchar();
