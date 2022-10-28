20551c257090af1fe3d8332fc6910f9ba7202088
Thomas Wagner <thomas.wagner@pa-systems.de>
2022-10-28 11:23:17 +0200

    no pointer iteration

--- examples/src/examples-ws22/main.c
+++ examples/src/examples-ws22/main.c
@@ -3,8 +3,8 @@
 #include "lecture_examples1.h"
 
 int main(int argc, char* argv[]) {
-    for (int ii = 0; ii < argc; ii++, argv++) {
-        printf("arg %i: %s\n", ii, *argv);
+    for (int ii = 0; ii < argc; ii++) {
+        printf("arg %i: %s\n", ii, argv[ii]);
     }
 
     format_identifier();
