a4fb8c83f77ee7c9cd8f0a495913e6b1dee0cc5d
Thomas Wagner <thomas.wagner@pa-systems.de>
2022-11-04 16:00:28 +0100

    example 04.11.22

--- examples/src/examples-ws22/main.c
+++ examples/src/examples-ws22/main.c
@@ -2,16 +2,30 @@
 
 #include "lecture_examples1.h"
 
+
+#define SIZE 5
+
 int main(int argc, char* argv[]) {
-    for (int ii = 0; ii < argc; ii++) {
-        printf("arg %i: %s\n", ii, argv[ii]);
+    int my_arr[SIZE] = { 0 };
+    int my_arr2[SIZE] = { 0 };
+
+    for (int i = 0, j = SIZE - 1; i < SIZE; i++, j--) {
+        printf("index %d: %d\n", i, my_arr[i] + my_arr2[j]);
     }
 
-    format_identifier();
-    loops();
+    int i = 0;
+    int j = SIZE;
+    while (i < SIZE) {
+        printf("index %d: %d\n", i, my_arr[i] + my_arr2[j]);
+        i++;
+        j--;
+    }
 
-    int ii;
-    scanf_s(" %i", &ii);
+    int j = 0;
+    do {
+        printf("index %d: %d\n", j, my_arr[j]);
+        j++;
+    } while (j < SIZE);
 
     return 0;
 }
