b67701d3773d643253790e794525ab5a5167b742
Thomas Wagner <thomas@the-wagner.de>
2023-05-12 13:49:32 +0200

    flags example

--- examples/src/examples-ws22/main.c
+++ examples/src/examples-ws22/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+#define READ 0x1
+#define WRITE 0x2
+#define COPY 0x4
+#define DELETE 0x8
+#define READ_WRITE READ | WRITE
+#define MOVE COPY | DELETE
+
 int main(int argc, char* argv[]) {
+    char user_rights = READ_WRITE | COPY;
+
+    printf("%s READ\n", user_rights & READ ? "CAN" : "CANNOT");
+    printf("%s WRITE\n", user_rights & WRITE ? "CAN" : "CANNOT");
+    printf("%s COPY\n", user_rights & COPY ? "CAN" : "CANNOT");
+    printf("%s DELETE\n", user_rights & DELETE ? "CAN" : "CANNOT");
+
     printf("Press ENTER to Continue\n");
     int ch = getchar();
 
