1c7cdf2a4bad9c2278f0a7bab29ea796dfb9f6eb
Thomas Wagner <thomas.wagner@pa-systems.de>
2024-12-13 12:23:44 +0100

    bump

--- examples/uebung-informatik-examples/can_decoding/main.c
+++ examples/uebung-informatik-examples/can_decoding/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-#include "can_decoder.h"
+//#include "can_decoder.h"
+
+double decode_signal(char frame[], int bit_pos, int bit_len, double lin_factor, double lin_offset);
 
 int main() {
     char can_frame[] = { 0x3D, 0xD7, 0x13, 0xD7, 0x14, 0x0E, 0x73, 0x72 };
