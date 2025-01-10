c46cd50528922506ea50adc89254d08091bbf92e
Thomas Wagner <thomas.wagner@pa-systems.de>
2025-01-10 16:58:56 +0100

    bump

--- examples/uebung-informatik-examples/can_decoding/main.c
+++ examples/uebung-informatik-examples/can_decoding/main.c
@@ -7,17 +7,16 @@ double decode_signal_be(unsigned char frame[], int bit_pos, int bit_len, double 
 double decode_signal_le(unsigned char frame[], int bit_pos, int bit_len, double lin_factor, double lin_offset);
 
 int main() {
-    int a = 0xE6 << 8;
-
-    unsigned char can_frame_be[] = { 0x3D, 0xD7, 0x13, 0xD7, 0x14, 0x0E, 0x73, 0x72 };
+    unsigned char can_frame_le[] = { 0x3D, 0xD7, 0x13, 0xD7, 0x14, 0x0E, 0x73, 0x72 };
+    unsigned char can_frame_be[] = { 0x3D, 0xD7, 0x13, 0xD7, 0x14, 0x1C, 0xE6, 0x7F };
 
     // decode signal 'MO_Hubraum'
-    double displacement_be = decode_signal_be(can_frame_be, 40, 7, 0.1, 0);
+    double displacement_le = decode_signal_le(can_frame_be, 40, 7, 0.1, 0);
     // decode signal 'MO_Leistung'
-    double power_be = decode_signal_be(can_frame_be, 48, 9, 1, 0);
+    double power_le = decode_signal_le(can_frame_be, 48, 9, 1, 0);
 
-    printf("displacement (be): %f l\n", displacement_be);
-    printf("power (be): %f kW\n", power_be);
+    printf("displacement (le): %f l\n", displacement_le);
+    printf("power (le): %f kW\n", power_le);
 
     return 0;
 }
