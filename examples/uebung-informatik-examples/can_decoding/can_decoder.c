fc3a2f09cb5f4f910e2890063a84d7b1c3aefd3b
Thomas Wagner <thomas@the-wagner.de>
2025-01-10 14:55:30 +0100

    hm

--- examples/uebung-informatik-examples/can_decoding/can_decoder.c
+++ examples/uebung-informatik-examples/can_decoding/can_decoder.c
@@ -1,6 +1,6 @@
 #include <math.h>
 
-double decode_signal(char frame[], int bit_pos, int bit_len, double lin_factor, double lin_offset) {
+double decode_signal_be(unsigned char frame[], int bit_pos, int bit_len, double lin_factor, double lin_offset) {
     int byte_pos = (int)floor(bit_pos / 8.0);
     int byte_len = (int)ceil(bit_len / 8.0);
 
@@ -15,6 +15,29 @@ double decode_signal(char frame[], int bit_pos, int bit_len, double lin_factor, 
 
     raw_value >>= shift;
     raw_value &= mask;
+
+    return raw_value * lin_factor + lin_offset;
+}
+
+double decode_signal_le(unsigned char frame[], int bit_pos, int bit_len, double lin_factor, double lin_offset) {
+    int byte_pos = (int)floor(bit_pos / 8.0);
+    int byte_len = (int)ceil(bit_len / 8.0);
+
+    int raw_value = 0;
+
+    for (int pos = byte_pos, byte_shift = byte_len - 1; pos < byte_pos + byte_len; pos++, byte_shift--) {
+        char x = frame[pos];
+        int a = frame[pos];
+        int b = byte_shift * 8;
+        int c = a << b;
+        int d = raw_value | c;
+
+        raw_value |= frame[pos] << (byte_shift * 8);
+    }
+
+    int shift = 8 - bit_len % 8;
+
+    raw_value >>= shift;
 
     return raw_value * lin_factor + lin_offset;
 }
